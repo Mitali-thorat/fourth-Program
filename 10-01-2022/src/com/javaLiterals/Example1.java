692342e5dcff7727662ffe2a63f6b91314af6157
mital <mital@DESKTOP-PQQKCMJ>
2022-01-22 15:08:35 +0530

    fourth commit

---
+++ 10-01-2022/src/com/javaLiterals/Example1.java
@@ -0,0 +1,29 @@
+package com.javaLiterals;
+
+public class Example1
+
+ {
+
+	public static void main(String[] args )
+			{
+		
+	int productId = 2020;
+	String productName = "pepsi";
+	float productDate = 2022;
+	char productGread = 'A';
+	String productAddres = "new mumbai";
+	String productCompany = "pepsi";
+	float productPrice = 21;
+	boolean productQuality = true;// true means good and false means bad 
+	
+	System.out.println(productId);
+	System.out.println(productName );
+	System.out.println(productDate);
+	System.out.println(productGread );
+	System.out.println(productAddres);
+	System.out.println(productCompany);
+	System.out.println(productPrice);
+	System.out.println(productQuality );
+			}
+ }
+
